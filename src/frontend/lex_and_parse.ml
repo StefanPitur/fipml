7a52b193604125eaed0ac4779b26e8ccdfe08c88
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2023-12-12 00:43:54 +0200

    Parser & Lexer: Exception Handling
    The commit try-catches error that may occur during the Lexing or
    Parsing phase, providing useful information in the terminal about
    the encountered error, so it may be diagnosted.

--- src/frontend/lex_and_parse.ml
+++ src/frontend/lex_and_parse.ml
@@ -1,1 +1,15 @@
-let parse_source_code (lexbuf : Lexing.lexbuf) = Parser.program Lexer.token lexbuf;;
+open Lexing
+open Core
+
+let print_position outx lexbuf =
+  let pos = lexbuf.lex_curr_p in
+  fprintf outx "%s:%d:%d" pos.pos_fname
+    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+
+let parse_source_code_with_error (lexbuf : lexbuf) = 
+  try Parser.program Lexer.token lexbuf with
+  | Lexer.LexerError msg -> 
+    fprintf stderr "%a: %s\n" print_position lexbuf msg
+  | Parser.Error ->
+    fprintf stderr "%a: Syntax Error\n" print_position lexbuf;
+    exit(-1)
