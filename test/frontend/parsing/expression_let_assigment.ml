89a2719f30cc7f6c2ce20f69f8464e262f7c36b5
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2024-01-06 00:13:28 +0200

    Expect Tests for expression let-assigment

---
+++ test/frontend/parsing/expression_let_assigment.ml
@@ -0,0 +1,8 @@
+let%expect_test "expression: let assigment" =
+  let source_code = "let x = expr1 in expr2" in
+  Pprint_parser_ast.pprint_parser_ast source_code;
+  [%expect {|
+    Program
+        Expr: Let var: x =
+            Expr: Var: expr1
+            Expr: Var: expr2 |}]
