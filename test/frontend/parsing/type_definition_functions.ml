6ed3b36d6328c00b00e1cf0e593a30383bfa207e
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2024-01-26 15:22:27 +0000

    Check option and arrow precendence for custom type

--- test/frontend/parsing/type_definition_functions.ml
+++ test/frontend/parsing/type_definition_functions.ml
@@ -1,7 +1,7 @@
 let%expect_test "custom type with function params" = 
   let source_code = "
     type custom_simple_type = 
-    | Constructor1 of int -> int -> unit
+    | Constructor1 of my_custom_type -> int -> unit
     | Constructor2 of int option -> unit -> int option
     | Constructor3 of (int -> unit) option
   " in
@@ -11,7 +11,7 @@ let%expect_test "custom type with function params" =
         Type Name: custom_simple_type
         Type Constructors:
             Type Constructor Name: Constructor1
-                Type Expr: (Int -> (Int -> Unit))
+                Type Expr: (my_custom_type -> (Int -> Unit))
             Type Constructor Name: Constructor2
                 Type Expr: (Int option -> (Unit -> Int option))
             Type Constructor Name: Constructor3
