fa3f5662ba6a3939fa97dcf84f6602379143012e
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2024-01-30 00:11:57 +0000

    Type Env - get constructor by name in interface

--- src/frontend/typing/type_envs.mli
+++ src/frontend/typing/type_envs.mli
@@ -16,3 +16,5 @@ val assert_custom_type_not_in_types_env : Ast_types.Type_name.t -> Ast_types.Typ
 
 val assert_constructor_in_constructors_env : Ast_types.Constructor_name.t -> constructor_env_entry list -> unit Or_error.t;;
 val assert_constructor_not_in_constructors_env : Ast_types.Constructor_name.t -> constructor_env_entry list -> unit Or_error.t;;
+
+val get_constructor_by_name : Ast_types.Constructor_name.t -> constructor_env_entry list -> constructor_env_entry Or_error.t
