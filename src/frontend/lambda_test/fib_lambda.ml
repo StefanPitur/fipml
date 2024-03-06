97141ad3fe42ed17ac37b6dfa29dd8b6b6ee00a6
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2024-03-06 01:55:40 +0000

    gauss in lambda

--- src/frontend/lambda_test/fib_lambda.ml
+++ src/frontend/lambda_test/fib_lambda.ml
@@ -1,25 +1,63 @@
 open Lambda
+
+let rec fib n = if n <= 0 then 0 else n + fib (n - 1)
+
 let fib_lambda =
   let fib = Ident.create_local "fib"
-  and n = Ident.create_local "n" in
+  and n = Ident.create_local "n"
+  and x = Ident.create_local "x" in
   let body =
-    lfunction
-      ~kind:Curried
-      ~params:[n, Pgenval]
-      ~return:Pgenval
-      ~loc:Loc_unknown
-      ~attr:default_function_attribute
+    lfunction ~kind:Curried
+      ~params:[ (n, Pgenval) ]
+      ~return:Pgenval ~loc:Loc_unknown ~attr:default_function_attribute
       ~body:
-        (Lprim (Pintcomp Clt, [Lconst (Const_base (Const_int 0)); Lvar n], Loc_unknown))
+        (Lifthenelse
+           ( Lprim
+               ( Pintcomp Cle,
+                 [ Lvar n; Lconst (Const_base (Const_int 0)) ],
+                 Loc_unknown ),
+             Lconst (Const_base (Const_int 0)),
+             Llet
+               ( Strict,
+                 Pgenval,
+                 x,
+                 Lprim
+                   ( Psubint,
+                     [ Lvar n; Lconst (Const_base (Const_int 1)) ],
+                     Loc_unknown ),
+                 Lprim
+                   ( Paddint,
+                     [
+                       Lvar n;
+                       Lapply
+                         {
+                           ap_func = Lvar fib;
+                           ap_args = [ Lvar x ];
+                           ap_loc = Loc_unknown;
+                           ap_tailcall = Default_tailcall;
+                           ap_inlined = Default_inline;
+                           ap_specialised = Default_specialise;
+                         };
+                     ],
+                     Loc_unknown ) ) ))
   in
   Lprim
-    (Psetglobal (Ident.create_persistent "Fib"),
-     [Lletrec ([fib, body], Lprim (Pmakeblock (0, Immutable, Some [Pgenval]), [Lvar fib], Loc_unknown))], Loc_unknown)
+    ( Psetglobal (Ident.create_persistent "Fib"),
+      [
+        Lletrec
+          ( [ (fib, body) ],
+            Lprim
+              ( Pmakeblock (0, Immutable, Some [ Pgenval ]),
+                [ Lvar fib ],
+                Loc_unknown ) );
+      ],
+      Loc_unknown )
 
 let write_fib () =
   let fd = open_out "fib.cmo" in
   (* Turn the lambda code into a sequence of instructions *)
   let instructions = Bytegen.compile_implementation "Fib" fib_lambda in
   (* Write the sequence of instructions to the file revacc.cmo *)
-  Emitcode.to_file fd "Fib" "fib.cmo" ~required_globals:Ident.Set.empty instructions;
+  Emitcode.to_file fd "Fib" "fib.cmo" ~required_globals:Ident.Set.empty
+    instructions;
   close_out fd
--- src/frontend/lambda_test/revacc_lambda.ml
+++ src/frontend/lambda_test/revacc_lambda.ml
@@ -1,7 +1,6 @@
 open Lambda
 
-type 'a mlist = Nil
-              | Cons of { mutable hd: 'a; mutable tl: 'a mlist }
+type 'a mlist = Nil | Cons of { mutable hd : 'a; mutable tl : 'a mlist }
 
 let rev_acc_lambda =
   let reverse_acc = Ident.create_local "reverse_acc"
@@ -9,34 +8,50 @@ let rev_acc_lambda =
   and tl = Ident.create_local "tl"
   and acc = Ident.create_local "acc" in
   let body =
-    lfunction
-      ~kind:Curried
-      ~params:[xs, Pgenval; acc, Pgenval]
-      ~return:Pgenval
-      ~loc:Loc_unknown
-      ~attr:default_function_attribute
+    lfunction ~kind:Curried
+      ~params:[ (xs, Pgenval); (acc, Pgenval) ]
+      ~return:Pgenval ~loc:Loc_unknown ~attr:default_function_attribute
       ~body:
-      (Lifthenelse
-          (Lvar xs,
-          Llet (Strict, Pgenval, tl, (Lprim (Pfield (1, Pointer, Mutable), [Lvar xs], Loc_unknown)),
-                Lsequence (Lprim (Psetfield (1, Pointer, Assignment), [Lvar xs; Lvar acc], Loc_unknown),
-                            Lapply { ap_func = Lvar reverse_acc;
-                                    ap_args = [Lvar tl; Lvar xs];
-                                    ap_loc = Loc_unknown;
-                                    ap_tailcall = Default_tailcall;
-                                    ap_inlined = Default_inline;
-                                    ap_specialised = Default_specialise })),
-          Lvar acc))
+        (Lifthenelse
+           ( Lvar xs,
+             Llet
+               ( Strict,
+                 Pgenval,
+                 tl,
+                 Lprim (Pfield (1, Pointer, Mutable), [ Lvar xs ], Loc_unknown),
+                 Lsequence
+                   ( Lprim
+                       ( Psetfield (1, Pointer, Assignment),
+                         [ Lvar xs; Lvar acc ],
+                         Loc_unknown ),
+                     Lapply
+                       {
+                         ap_func = Lvar reverse_acc;
+                         ap_args = [ Lvar tl; Lvar xs ];
+                         ap_loc = Loc_unknown;
+                         ap_tailcall = Default_tailcall;
+                         ap_inlined = Default_inline;
+                         ap_specialised = Default_specialise;
+                       } ) ),
+             Lvar acc ))
   in
-  Lprim (Psetglobal (Ident.create_persistent "Revacc"),
-          [Lletrec ([reverse_acc, body],
-                    Lprim (Pmakeblock (0, Mutable, Some [Pgenval]), [Lvar reverse_acc], Loc_unknown))],
-          Loc_unknown)
-  
+  Lprim
+    ( Psetglobal (Ident.create_persistent "Revacc"),
+      [
+        Lletrec
+          ( [ (reverse_acc, body) ],
+            Lprim
+              ( Pmakeblock (0, Mutable, Some [ Pgenval ]),
+                [ Lvar reverse_acc ],
+                Loc_unknown ) );
+      ],
+      Loc_unknown )
+
 let write_revacc () =
   let fd = open_out "revacc.cmo" in
   (* Turn the lambda code into a sequence of instructions *)
   let instructions = Bytegen.compile_implementation "Revacc" rev_acc_lambda in
   (* Write the sequence of instructions to the file revacc.cmo *)
-  Emitcode.to_file fd "Revacc" "revacc.cmo" ~required_globals:Ident.Set.empty instructions;
+  Emitcode.to_file fd "Revacc" "revacc.cmo" ~required_globals:Ident.Set.empty
+    instructions;
   close_out fd
