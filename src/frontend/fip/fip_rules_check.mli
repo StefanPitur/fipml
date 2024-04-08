cb3c6a768c9ca2b95bf5a407b1e6fc38e076bb9a
Stefan - Alexandru Pitur <stefan.pitur24@gmail.com>
2024-04-08 10:23:23 +0300

    fip rules test for values

--- src/frontend/fip/fip_rules_check.mli
+++ src/frontend/fip/fip_rules_check.mli
@@ -4,3 +4,6 @@ open Typing
 val fip_rules_check_expr :
   Typed_ast.expr -> BorrowedSet.t -> Functions_env.functions_env -> Fip_ast.expr
 (** Pass a fip/fbip function's body, checks if it is actually fip or not. *)
+
+val fip_rules_check_value : Typed_ast.value -> BorrowedSet.t -> Fip_ast.value
+(** Pass a fip/fbip value, checks if it is actually fip or not. *)
